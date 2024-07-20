9b003c1118868910874e291c9c72172370a1dd33
Mehdi Zain <mzainmehdi@yahoo.com>
2024-07-20 16:49:49 +0500

    display notes from storage and add new notes feature

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+showNotes();
 let a = document.getElementById("addBtn");
 a.addEventListener("click", function (e) {
     let txt = document.getElementById("newNoteText");
@@ -11,8 +12,35 @@ a.addEventListener("click", function (e) {
             notesObj = JSON.parse(notes);
         }
         notesObj.push(txt.value);
-        console.log(txt.value);
         localStorage.setItem("notes", JSON.stringify(notesObj));
         txt.value = "";
+        showNotes();
     }
 })
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    let html = "";
+    let notesObj = [];
+    if (notes == null) {
+        notesObj = [];
+        html = `<div class="alert alert-warning" role="alert">
+  No notes added yet!<br> Click on Add Note button to add notes!
+</div>
+`;
+        let notesContainer = document.getElementById("notes");
+        notesContainer.innerHTML = html;
+    }
+    else {
+        notesObj = JSON.parse(notes);
+        notesObj.forEach(function (element, index) {
+            html += `<div class="card my-2 mx-2" style="width: 14rem;">
+                <div class="card-body">
+                    <p class="card-text">${element}</p>
+                    <a class="btn btn-primary">Delete Note</a>
+                </div>
+            </div>`;
+        })
+        let notesContainer = document.getElementById("notes");
+        notesContainer.innerHTML = html;
+    }
+}
